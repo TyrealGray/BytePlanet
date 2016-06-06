e07f431e54a97dc3ea15c6ee94b49371d4bf2010
Tyreal Gray <tyrealgray@Tyreals-MacBook-Pro.local>
2016-06-06 14:26:22 +0800

    add keyword

--- js/module/component/DiaryManager.js
+++ js/module/component/DiaryManager.js
@@ -2,12 +2,16 @@ define(function (require, exports) {
     'use strict';
 
     var englishDiaries = [
-        { index: 1, title: 'export 3dmax model to UE4', date: '2015.05.10' },
-        { index: 3, title: 'module programming in UE4', date: '2015.05.22' }
+        { index: 1, title: 'export 3dmax model to UE4', keyword:'3dmax model ue4' ,
+        date: '2015.05.10' },
+        { index: 3, title: 'module programming in UE4', keyword:'module ue4',
+        date: '2015.05.22' }
     ],
         chineseDiaries = [
-            { index: 0, title: '關於遊戲建模導入虛幻4', date: '2015.05.10' },
-            { index: 2, title: '虛幻4在開發中的模塊化編程', date: '2015.05.22' }
+            { index: 0, title: '關於遊戲建模導入虛幻4', keyword:'3dmax model ue4' ,
+            date: '2015.05.10' },
+            { index: 2, title: '虛幻4在開發中的模塊化編程',keyword:'模块化 ue4' , 
+            date: '2015.05.22' }
         ];
 
     function getList(language) {
