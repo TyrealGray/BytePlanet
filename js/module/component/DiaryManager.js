682427e1292f9974e9bc5cb4b747b0ebcd002a04
tyrealgray <tyrealgray@ymail.com>
2016-06-10 18:31:32 +0800

    add new english diary

--- js/module/component/DiaryManager.js
+++ js/module/component/DiaryManager.js
@@ -5,14 +5,16 @@ define(function (require, exports) {
         { index: 1, title: 'export 3dmax model to UE4', keyword:'3dmax model ue4' ,
         date: '2015.05.10' },
         { index: 3, title: 'module programming in UE4', keyword:'module ue4',
-        date: '2015.05.22' }
+        date: '2015.05.22' },
+        {index:5,title:'using jasmine in requirejs', keyword:'unit test requirejs jasmine javascript',
+            date:'2016.06.09'}
     ],
         chineseDiaries = [
             {index: 0, title: '關於遊戲建模導入虛幻4', keyword:'3dmax model ue4' ,
             date: '2015.05.10' },
             {index: 2, title: '虛幻4在開發中的模塊化編程',keyword:'模块化 ue4' , 
             date: '2015.05.22' },
-            {index:4,title:'在requirejs中使用jasmine', keyword:'requirejs jasmine javascript',
+            {index:4,title:'在requirejs中使用jasmine', keyword:'单元测试 requirejs jasmine javascript',
         date:'2016.06.09'}
   ];
 
