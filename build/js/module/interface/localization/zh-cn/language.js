d74a02409ff2dfc72a3e872b1b4d9fe788191f07
tyrealgray <tyrealgray@ymail.com>
2016-09-05 22:53:08 +0800

    build

--- build/js/module/interface/localization/zh-cn/language.js
+++ build/js/module/interface/localization/zh-cn/language.js
@@ -1,1 +1,1 @@
-define({LeftCanvas:{Home:"主页",MeInfo:"关于我",Diary:"日志",Language:"Language"},HomePage:{Send:"发送",Pause:"暂停",Cancel:"取消",Resume:"继续"},MeInfoPage:{Title:"我的名字叫Tyreal Gray",RecentlyMove:"9.13，德国我来了！"},DiariesPage:{Title:"标题",Date:"日期"},LanguageSettingPage:{Setting:"设置",Save:"保存"}});
+define({LeftCanvas:{Home:"主页",MeInfo:"关于我",Diary:"日志",Language:"Language"},HomePage:{Send:"发送",Pause:"暂停",Cancel:"取消",Resume:"继续"},MeInfoPage:{Title:"我的名字叫Tyreal Gray",RecentlyMove:"9.13，德国我来了！",Description:"爱游戏，爱编程"},DiariesPage:{Title:"标题",Date:"日期"},LanguageSettingPage:{Setting:"设置",Save:"保存"}});
