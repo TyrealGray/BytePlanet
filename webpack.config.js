126d434c97ad8d10bf5d3ca6e2dbca75d54dc18b
tyrealgray <tyrealgray@ymail.com>
2016-11-06 17:03:44 +0100

    use es6

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,18 @@ module.exports = {
         path: __dirname,
         filename: "bundle.js"
     },
+    devtool: 'source-map',
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0']
+                }
+            }
         ]
     }
 };
