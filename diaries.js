62a9dd98e4495d29d7f46f3b4e190277ead61aa4
tyrealgray <tyrealgray@ymail.com>
2016-11-27 14:23:11 +0100

    finished diaries page interface

--- diaries.js
+++ diaries.js
@@ -1,3 +1,26 @@
 import { initBytePlanetPage } from './assets/js/main';
 
 initBytePlanetPage();
+
+var tableBodyEl = document.getElementById("tableBody");
+
+var requestStories = new Request('assets/data/diaries.json'),
+    storiesEl = [];
+
+fetch(requestStories).then((response) => {
+    response.json().then(function (stories) {
+        console.log(stories);
+        for (var story of stories.en) {
+            storiesEl.push(`<tr>
+                                <td>${ story.title }</td>
+                                <td>${ story.date  }</td>
+                            </tr>`
+            );
+        }
+
+        tableBodyEl.innerHTML = storiesEl.join('');
+        console.log(storiesEl);
+    })
+});
+
+
