4257a00bc260ab038a1e47d1802073d8e0e7964c
tyrealgray <tyrealgray@ymail.com>
2016-11-29 22:57:58 +0100

    refact to es6

--- diaries.js
+++ diaries.js
@@ -11,7 +11,7 @@ fetch(requestStories).then((response) => {
 	response.json().then(function (stories) {
 		console.log(stories);
 		for (var story of stories.en) {
-			storiesEl.push(`<tr data-url="diary/${story.url}.html" >
+			storiesEl.push(`<tr url="diary/${story.url}.html" >
                                 <td>${ story.title}</td>
                                 <td>${ story.date}</td>
                             </tr>`);
@@ -19,15 +19,13 @@ fetch(requestStories).then((response) => {
 
 		tableBodyEl.innerHTML = storiesEl.join('');
 
-		// tableBodyEl.getElementsByTagNameNS('tr').map(function(e){
-		// 	console.log()
-		// })
+		Array.prototype.slice.call(tableBodyEl.getElementsByTagName('tr')).forEach(function (element) {
+			console.log(element)
+			document.addEventListener('onclick', () => {
+				location.href = this.getAttribute('url');
+			}, false)
+		}, this);
 
-		$(tableBodyEl).find('tr').each(function(){
-			$(this).click(function(){
-				location.href=$(this).data('url');
-			})
-		})
 	})
 });
 
