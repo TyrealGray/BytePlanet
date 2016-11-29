9d9c56befbf553964ddbc0528bc7b3af791298f0
tyrealgray <tyrealgray@ymail.com>
2016-11-29 22:16:06 +0100

    change file structure

--- diaries.js
+++ diaries.js
@@ -5,22 +5,21 @@ initBytePlanetPage();
 var tableBodyEl = document.getElementById("diariesTableBody");
 
 var requestStories = new Request('assets/data/diaries.json'),
-    storiesEl = [];
+	storiesEl = [];
 
 fetch(requestStories).then((response) => {
-    response.json().then(function (stories) {
-        console.log(stories);
-        for (var story of stories.en) {
-            storiesEl.push(`<tr>
-                                <td>${ story.title }</td>
-                                <td>${ story.date  }</td>
-                            </tr>`
-            );
-        }
+	response.json().then(function (stories) {
+		console.log(stories);
+		for (var story of stories.en) {
+			storiesEl.push(`<tr href="diary/${ story.url }.html">
+                                <td>${ story.title}</td>
+                                <td>${ story.date}</td>
+                            </tr>`);
+		}
 
-        tableBodyEl.innerHTML = storiesEl.join('');
-        console.log(storiesEl);
-    })
+		tableBodyEl.innerHTML = storiesEl.join('');
+		console.log(storiesEl);
+	})
 });
 
 
