12760a6bb9b6c3eae79d470845fbb50cf8425c8e
tyrealgray <tyrealgray@ymail.com>
2016-11-29 22:46:04 +0100

    at least now we have english diaries back online

--- diaries.js
+++ diaries.js
@@ -11,14 +11,23 @@ fetch(requestStories).then((response) => {
 	response.json().then(function (stories) {
 		console.log(stories);
 		for (var story of stories.en) {
-			storiesEl.push(`<tr href="diary/${ story.url }.html">
+			storiesEl.push(`<tr data-url="diary/${story.url}.html" >
                                 <td>${ story.title}</td>
                                 <td>${ story.date}</td>
                             </tr>`);
 		}
 
 		tableBodyEl.innerHTML = storiesEl.join('');
-		console.log(storiesEl);
+
+		// tableBodyEl.getElementsByTagNameNS('tr').map(function(e){
+		// 	console.log()
+		// })
+
+		$(tableBodyEl).find('tr').each(function(){
+			$(this).click(function(){
+				location.href=$(this).data('url');
+			})
+		})
 	})
 });
 
